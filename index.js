769dd84c57f8ce5705a40ba1028ca444a5f7bfc0
Ajit Kishor <t-ajkish@microsoft.com>
2018-05-29 19:57:18 +0530

    working

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
+'use strict';
+const http = require('http');
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const host = `http://api.apixu.com`;
+const ApiKey = `031e9ff47c244c51be165319182505`;
+const app = express(); 
 
-var request = require('request');
-var builder = require('botbuilder'); 
-var apiairecognizer = require('api-ai-recognizer'); 
-var request = require('request'); 
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector); 
-var recognizer = new apiairecognizer('bdafa2ddfd104affb00277dec16dbe94'); 
-var intents = new builder.IntentDialog({ recognizers: [recognizer] }); 
-bot.dialog('/',intents); 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-intents.matches('Weather',[ function(session,args){ var city = builder.EntityRecognizer.findEntity(args.entities,'cities'); 
-if (city)
-{ var city_name = city.entity; var url = "http://api.apixu.com/v1/current.json?key=031e9ff47c244c51be165319182505&q=" + city_name; 
-request(url,function(error,response,body){ body = JSON.parse(body); temp = body.current.temp_c; session.send("It's " + temp + " degrees celsius in " + city_name); }); }
-else
-{ builder.Prompts.text(session, 'Which city do you want the weather for?'); } }, 
-function(session,results){ var city_name = results.response; var url = "http://api.apixu.com/v1/current.json?key=031e9ff47c244c51be165319182505&q=" + city_name; 
-request(url,function(error,response,body){ body = JSON.parse(body); temp = body.current.temp_c; session.send("It's " + temp + " degrees celsius in " + city_name); }); } ]);
+app.use(bodyParser.json());
 
-intents.matches('BookMovie',function(session, args){ var fulfillment = builder.EntityRecognizer.findEntity(args.entities, 'fulfillment');
-if (fulfillment){ var speech = fulfillment.entity; session.send(speech); }else{ session.send('Sorry...not sure how to respond to that'); } });
+app.post('/webhook', function (req, res) {
+  // Get the city and date from the request
+  let city = req.body.queryResult.parameters['geo-city']; // city is a required param
+  let w = getWeather(city);
+  //console.log(w);
+  let response = " ";
+  //console.log(response);
+  let responseObj = {
+                      "fulfillmentText":response,
+                      "fulfillmentMessages":[{"text" :{"text": [w]}}],
+                      "source":""
+                    }
+  
+  //console.log(responseObj);
+  return res.json(responseObj);
+})
 
-intents.matches('smalltalk.greetings',function(session, args){ var fulfillment = builder.EntityRecognizer.findEntity(args.entities, 'fulfillment');
-if (fulfillment){ var speech = fulfillment.entity; session.send(speech); }else{ session.send('Sorry...not sure how to respond to that'); } });
+function getWeather (city) {
+    const wwoApiKey = '031e9ff47c244c51be165319182505';
+    let url = `http://api.apixu.com/v1/current.json?key=${wwoApiKey}&q=${city}`; 
 
-intents.onDefault(function(session){ session.send("Sorry...can you please rephrase?"); });
+    request(url, function (err, response, body) {
+      if(err){
+        console.log('error:', error);
+      } else {
+       let weather = JSON.parse(body);
+      // console.log(weather);
+       let message = `It's ${weather.current.temp_c} degrees in ${weather.location.name}!`;
+       
+       console.log(message);
+       return message;       
+      }
+    });
+}
+
+app.listen((process.env.PORT || 8000), () => {
+  console.log("Server is up and running...");
+});
+
