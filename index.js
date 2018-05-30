5d6868a8eec1da059a6a6209f0a856395d4c50fb
Ajit Kishor <t-ajkish@microsoft.com>
2018-05-30 10:43:48 +0530

    working webapp

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 const http = require('http');
 const request = require('request');
 const express = require('express');
@@ -16,39 +16,46 @@ app.use(bodyParser.json());
 app.post('/webhook', function (req, res) {
   // Get the city and date from the request
   let city = req.body.queryResult.parameters['geo-city']; // city is a required param
-  let w = getWeather(city);
-  //console.log(w);
+  let message;
+  var w = getWeather(city,message);
+ 
   let response = " ";
-  //console.log(response);
+ 
   let responseObj = {
-                      "fulfillmentText":response,
-                      "fulfillmentMessages":[{"text" :{"text": [w]}}],
-                      "source":""
+                      fulfillmentText: response,
+                      fulfillmentMessages:[{text :{text: [w]}}],
+                      source:""
                     }
   
   //console.log(responseObj);
   return res.json(responseObj);
 })
 
-function getWeather (city) {
-    const wwoApiKey = '031e9ff47c244c51be165319182505';
-    let url = `http://api.apixu.com/v1/current.json?key=${wwoApiKey}&q=${city}`; 
+let result;
 
-    request(url, function (err, response, body) {
-      if(err){
-        console.log('error:', error);
-      } else {
-       let weather = JSON.parse(body);
-      // console.log(weather);
-       let message = `It's ${weather.current.temp_c} degrees in ${weather.location.name}!`;
-       
-       console.log(message);
-       return message;       
-      }
-    });
+function cb(err,response,body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+  let weather =  JSON.parse(body); 
+   result  =  `It's ${weather.current.temp_c} degrees in ${weather.location.name}!`;
+   console.log(result);
+}
 }
 
-app.listen((process.env.PORT || 8000), () => {
+
+function getWeather (city,message) {
+    result = undefined;
+    const wwoApiKey = '031e9ff47c244c51be165319182505';
+    let url = `http://api.apixu.com/v1/current.json?key=${wwoApiKey}&q=${city}`; 
+    let req = request(url, cb);
+    while(result === undefined){
+        require('deasync').runLoopOnce();
+    }
+    return result;
+}
+
+app.listen((process.env.PORT || 8080), () => {
   console.log("Server is up and running...");
 });
 
