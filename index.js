ef35c17cd81ec51facb3beab5b21e43d42a086d4
Ajit Kishor <ajit.cr7kishor@gmail.com>
2018-06-13 21:21:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -388,17 +388,7 @@ else{
     let responseObj = {
                       fulfillmentText: response,
                       fulfillmentMessages:[{
-                        "card": {
-                        "title": "card title",
-                        "subtitle": "card text",
-                        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                        "buttons": [
-                          {
-                            "text": "button text",
-                            "postback": "https://assistant.google.com/"
-                          }
-                        ]
-                      }}],
+                        text :{text: [info]}}],
                       source:"",
                       payload : info
                     }
