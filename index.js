8802344a84c6a907cd76d8bdf924e4a76db84300
Ajit Kishor <t-ajkish@microsoft.com>
2018-06-14 16:29:39 +0530

    reco

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ else{
   {
     response = "Here's some recommendation for you";
     let responseObj = {
-                      fulfillmentText: response,
+                      fulfillmentText: JSON.stringify(info),
                       fulfillmentMessages:[
                         {text :{text: [JSON.stringify(info)]}}
                       ],
